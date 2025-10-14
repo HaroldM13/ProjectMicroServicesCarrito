9a75317461d693ee36547f4afaff6efc9b77b77e
HaroldM13 <119902485+HaroldM13@users.noreply.github.com>
2025-10-13 21:32:39 -0500

    funcionActualizarStock
    En esta parte se trabajo en la actualización del stock, tenemos una condición si el monto ingresado es menor al que esta en el stock en la db se debe de restar, si es mayor se suma, se agrego esta función en el controlador de productos en la carpeta Api, se elimino mensajes de las excepciones ya que podría mostrar información sensible, esta actualización se manejo con patch ya que solo es necesario actualizar o modificar 1 solo campo de la tabla

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,9 @@ use App\http\Controllers\Api\ProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return 'texto';
-});
-
 Route::apiResource('productos', ProductoController::class);
 
 // Direccion de paginaciones -- Juan_perez
 Route::get('productos/paginar/{cantidad}', [ProductoController::class, 'paginarSimple']);
 Route::get('productos/paginar/{cantidad}/pagina/{pagina}', [ProductoController::class, 'paginarAvanzada']);
+Route::patch('productos/{id}/actualizar-stock', [ProductoController::class, 'actualizarStock']);
