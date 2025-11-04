e60d9acab9673de304c1d40f22efd61d66df02c3
HaroldM13 <119902485+HaroldM13@users.noreply.github.com>
2025-11-04 14:56:38 -0500

    Commit antes de instalar todo para agregar JWT a los usuarios

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,12 @@ use App\http\Controllers\Api\ProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('productos', ProductoController::class);
+// Route::apiResource('productos', ProductoController::class);
+Route::get('productos', [ProductoController::class, 'index']);
+Route::get('productos/{id}', [ProductoController::class, 'show']);
+Route::post('productos', [ProductoController::class, 'store']);
+Route::put('productos/{id}', [ProductoController::class, 'update']);
+Route::delete('productos/{id}', [ProductoController::class, 'destroy']);
 
 // Direccion de paginaciones -- Juan_perez
 Route::get('productos/paginar/{cantidad}', [ProductoController::class, 'paginarSimple']);
